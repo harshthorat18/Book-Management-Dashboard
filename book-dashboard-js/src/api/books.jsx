4b15f074c1a03380031a19b74cb7d55c0438b8e3
harsh <harshthorat18@gmail.com>
2025-06-12 19:14:51 +0530

    Connected frontend to live backend

--- book-dashboard-js/src/api/books.jsx
+++ book-dashboard-js/src/api/books.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/books';
+const API_URL = "https://book-management-dashboard-lhug.onrender.com/books";
+
 
 export const getBooks = () => axios.get(API_URL);
 export const deleteBook = (id) => axios.delete(`${API_URL}/${id}`);
