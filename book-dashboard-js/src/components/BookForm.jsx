1bbf0ea7e8029cc411ac024cc65841c8ea670b78
harsh <harshthorat18@gmail.com>
2025-06-12 22:56:02 +0530

    Update API URL to Render backend

--- book-dashboard-js/src/components/BookForm.jsx
+++ book-dashboard-js/src/components/BookForm.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const API_URL = import.meta.env.DEV
   ? "http://localhost:3001/books"
-  : "https://book-management-dashboard-lhug.onrender.com/books";
+  : "https://book-management-backend-grlr.onrender.com/books";
 
-  
+
 export default function BookForm({ onAdd }) {
   const {
     register,
